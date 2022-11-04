0785b7520e4c45ca72cf170aead967af67853e92
pagnito <pagnito@gmail.com>
2022-11-04 01:43:30 -0400

    wait for db creation before creating table

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,17 +1,21 @@
 const knex = require('knex');
 const createCarsTable = require('./schemas/cars');
 const { PGHOST, PGUSER, PGPASSWORD, PGDATABASE, PGPORT } = process.env;
+
 const connection = knex({
-    client: 'pg',
-    connection: {
-        host: PGHOST,
-        port: PGPORT,
-        user: PGUSER,
-        password: PGPASSWORD,
-        database: PGDATABASE
-    }
+  client: 'pg',
+  connection: {
+    host: PGHOST,
+    port: PGPORT,
+    user: PGUSER,
+    password: PGPASSWORD,
+    database: PGDATABASE
+  }
 });
 
-createCarsTable(connection);
+setTimeout(() =>{
+  createCarsTable(connection);
+  //Didnt have time to debug, how to wait to make sure database is created before creating tables
+}, 2000)
 
 module.exports = connection;
